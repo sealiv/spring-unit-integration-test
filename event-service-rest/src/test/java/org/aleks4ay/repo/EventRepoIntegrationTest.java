6a22f75b5abb0c0da2649291b73078c983a138cc
Sealiv <sai.79.ua@gmail.com>
2022-06-14 14:07:12 +0300

    Changed tool for test repository from TestEntityManager to JdbcTemplate.

--- event-service-rest/src/test/java/org/aleks4ay/repo/EventRepoIntegrationTest.java
+++ event-service-rest/src/test/java/org/aleks4ay/repo/EventRepoIntegrationTest.java
@@ -1,42 +1,94 @@
 package org.aleks4ay.repo;
 
 import org.aleks4ay.dto.Event;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@DisplayName("Test for unique repository method in 'EventRepo.class'")
+@DisplayName("Test for all repository method in 'EventRepo.class'")
 public class EventRepoIntegrationTest {
 
+    private Event event;
+
     @Autowired
-    private TestEntityManager entityManager;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private EventRepo eventRepo;
 
+    @BeforeEach
+    public void init(){
+        event = new Event("title_repo", "place_repo", "Anatoly", "Lecture_repo",
+                LocalDateTime.of(2022, 5 , 5, 17, 17 ,17));
+    }
+
     @Test
-    public void getAllEventsByTitle() {
-        Event newEvent = new Event("title 11", "place one", "Anatoly", "Lecture",
-                LocalDateTime.of(2022, 1 , 1, 12, 0 ,1));
-        entityManager.persist(newEvent);
-        entityManager.flush();
+    public void getEventTest() {
+        jdbcTemplate.update("INSERT INTO event (id, title, place, speaker, event_type, date_time) values(? ,?, ?, ?, ?, ?);",
+                8L, "title_8", "place for jdbcTemplate", "Anatoly", "Lecture", LocalDateTime.now());
+        Event eventFromDB = eventRepo.findById(8L).orElse(null);
+        assertNotNull(eventFromDB);
+    }
 
+    @Test
+    public void getAllEventsTest() {
+        jdbcTemplate.update("INSERT INTO event (id, title, place, speaker, event_type, date_time) values(? ,?, ?, ?, ?, ?);",
+                1L, "title_1", "place for jdbcTemplate", "Anatoly", "Lecture", LocalDateTime.now());
+        jdbcTemplate.update("INSERT INTO event (id, title, place, speaker, event_type, date_time) values(? ,?, ?, ?, ?, ?);",
+                2L, "title_2", "place for jdbcTemplate", "Anatoly", "Lecture", LocalDateTime.now());
+        List<Event> eventsFromDB = new ArrayList<>();
+        eventRepo.findAll().forEach(eventsFromDB::add);
+
+        assertAll(()-> assertEquals("title_1", eventsFromDB.get(0).getTitle()),
+                  ()-> assertEquals(2, eventsFromDB.size()));
+    }
+
+    @Test
+    public void getAllEventsByTitleTest() {
+        jdbcTemplate.update("INSERT INTO event (id, title, place, speaker, event_type, date_time) values(? ,?, ?, ?, ?, ?);",
+                1L, "title 11", "place for jdbcTemplate", "Anatoly", "Lecture", LocalDateTime.now());
         List<Event> foundEvent = eventRepo.getAllEventsByTitle("title 11");
+        assertEquals("place for jdbcTemplate", foundEvent.get(0).getPlace());
+    }
 
-        assertThat(foundEvent.get(0).getTitle(), is(equalTo("title 11")));
+    @Test
+    public void createEventTest() {
+        eventRepo.save(event);
+        Event eventFromDB = jdbcTemplate.queryForObject("SELECT * FROM event WHERE title = ?;",
+                new BeanPropertyRowMapper<>(Event.class), event.getTitle());
+        assertNotNull(eventFromDB);
+    }
+
+    @Test
+    @Sql(scripts = {"classpath:initTest.sql"})
+    public void deleteEventTest() {
+        Event eventFromDBBeforeDeleting = jdbcTemplate.queryForObject("SELECT * FROM event WHERE title = ?;",
+                new BeanPropertyRowMapper<>(Event.class), "title 5");
+        long deletingEventId = eventFromDBBeforeDeleting.getId();
+
+        eventRepo.delete(eventFromDBBeforeDeleting);
+        eventRepo.findAll();
+
+        List<Event> eventFromDBAfterDeleting = jdbcTemplate.query("SELECT * FROM event;", new BeanPropertyRowMapper<>(Event.class));
+        Event foundEvent = eventFromDBAfterDeleting.stream()
+                .filter(event -> event.getId() == deletingEventId)
+                .findFirst()
+                .orElse(null);
+        assertNull(foundEvent);
     }
 }
